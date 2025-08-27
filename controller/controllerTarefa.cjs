2afb8bc19b8f6318f59f5dc38de006b20592f713
Matheus Torrão <matheustorraox@gmail.com>
2025-08-27 11:52:01 -0300

    feat: Adicionando readline para leitura de terminal

--- controller/controllerTarefa.cjs
+++ controller/controllerTarefa.cjs
@@ -1,16 +1,41 @@
-const tarefas = require('../model/tarefas.cjs');
+const { resolve } = require('path');
+const readline = require('readline');
 
-const InserirDados = async function (dadosTarefa) {
-    let result = await tarefas.inserttarefa(dadosTarefa)
+const model = require('../model/tarefas.json');
 
-    if (result){
-        return true
-    }else{
-        return false
-    }
-    
-};
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function askQuestion (query){
+    return new Promise (resolve =>{
+        rl.question(query,resolve)
+    });
+}
+
+async function questUser() {
+    const opcao = await askQuestion('Digite uma Opção (1/4): ');
+    rl.close();
+}
+const menu = async function appMenu () {
+    const opces = ['1 - Criar tarefa',
+        '2 - Visualizar Tarefa', 
+        '3 - Atualizar Tarefa', 
+        '4 - Excluir Tarefa']
+    console.log('Menu de Opções:');
+    console.log(opces)
+}
+
+const updateUser = async function Update () {
+    const nometarefa = await askQuestion('Digite o nome da Tarefa:'); 
+    console.log('chegou aqui')
+    rl.close();
+}
 
 module.exports = {
-    InserirDados
+    askQuestion,
+    questUser,
+    menu,
+    updateUser
 }
