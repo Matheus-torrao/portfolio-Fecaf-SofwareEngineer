8c841b9f02636b532276890848c5a4cb54095306
Matheus Torrão <matheustorraox@gmail.com>
2025-08-31 01:52:24 -0300

    Feat: Adicionando Novas funcionalidades para o crud

--- controller/controllerTarefa.cjs
+++ controller/controllerTarefa.cjs
@@ -2,6 +2,11 @@ const { resolve } = require('path');
 const readline = require('readline');
 
 const model = require('../model/tarefas.json');
+const { execPath } = require('process');
+const { error } = require('console');
+
+const tarefas = [];
+
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -15,19 +20,107 @@ function askQuestion (query){
 }
 
 async function questUser() {
-    const opcao = await askQuestion('Digite uma Opção (1/4): ');
-    rl.close();
-}
-const menu = async function appMenu () {
-    const opces = ['1 - Criar tarefa',
-        '2 - Visualizar Tarefa', 
-        '3 - Atualizar Tarefa', 
-        '4 - Excluir Tarefa']
-    console.log('Menu de Opções:');
-    console.log(opces)
-}
+    try{
+        const opcao = await askQuestion('Digite uma Opção (1/4): ');
+        const result = Number(opcao);
 
-const updateUser = async function Update () {
+        if(isNaN(opcao)) {
+            throw new error('Valor Digitado não é um número Válido!')
+        }
+    console.log("Você escolheu:", opcao);
+    } catch (error) {
+      console.error("Ocorreu um erro:", error.message);
+    } finally {
+      rl.close(); 
+    }
+  }
+
+const menu = async function appMenu () {
+    const opcoes = [
+        "1 - Criar tarefa",
+        "2 - Visualizar tarefa",
+        "3 - Atualizar tarefa",
+        "4 - Excluir tarefa",
+        "5 - Sair",
+      ];
+    
+      console.log("\n===== Menu de Opções =====\n");
+      opcoes.forEach((op) => console.log(op));
+    
+      try {
+        const resposta = await askQuestion("\nDigite uma opção: ");
+        const opcao = Number(resposta);
+    
+        switch (opcao) {
+
+          case 1:
+            const novaTarefa = await askQuestion('Digite a descrição da Tarefa: ')
+            tarefas.push(novaTarefa);
+            console.log('Tarefa Adicionada com sucesso!')
+            break;
+
+          case 2:
+            console.log(" Visualizando tarefas...");
+            if (tarefas.length === 0){
+                console.log('Nenhuma tarefa Cadastrada')
+            }else{
+                tarefas.forEach((t, i) => console.log(`${i + 1} - ${t}`))
+            }
+            break;
+
+          case 3:
+            console.log(" Atualizando tarefa...");
+            if (tarefas.length === 0 ){
+                console.log('Nenhuma tarefa para ser atualizada!')
+            }else{
+                console.log('\nEscolha a Tarefa para Atualizar: ');
+                tarefas.forEach((t,i) => console.log(`${i + 1} - ${t}`));
+                const indexUpdate = Number(await askQuestion('Número da Tarefa: ')) - 1;
+                if(indexUpdate >= 0 && indexUpdate <= tarefas.length){
+                    const novaDescricao = await askQuestion('Digite a nova Descrição: ');
+                    tarefas[indexUpdate] = novaDescricao;
+                    console.log('Tarefa Atualizada com Sucesso!')
+                }else{
+                    console.log('Número Inválido')
+                }
+            }
+            break;
+            
+          case 4:
+            console.log(" Excluindo tarefa...");
+            if(tarefas.length === 0 ){
+                console.log('Nenhuma tarefa para excluir.');
+                break;
+            }
+            console.log('\nEscolha a tarefa para excluir');
+            tarefas.forEach((t,i) => console.log(`${i + 1 } - ${t}`));
+            const indexDelete = Number(await askQuestion('Número da tarefa: ')) - 1;
+            if (indexDelete >= 0 && indexDelete < tarefas.length){
+                tarefas.splice(indexDelete, 1);
+                console.log('Tarefa removida com sucesso!');
+            }else{
+                console.log('Número inválido!')
+            }
+            break;
+
+          case 5:
+            console.log(" Saindo do programa.");
+            rl.close();
+            return;
+
+          default:
+            console.log(" Opção inválida!");
+        }
+
+        menu();
+    
+      } catch (error) {
+        console.error("Erro:", error.message);
+        rl.close();
+      }
+    };
+
+const upgradeTarefa = async function Update (){
     const nometarefa = await askQuestion('Digite o nome da Tarefa:'); 
     console.log('chegou aqui')
     rl.close();
@@ -37,5 +130,4 @@ module.exports = {
     askQuestion,
     questUser,
     menu,
-    updateUser
 }
